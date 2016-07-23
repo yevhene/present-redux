04016426448a3dbbf587b78f5580891566e9279d
Yevhen Shemet <yevhene@gmail.com>
2016-07-24 02:14:31 +0300

    Presentation moved in named folder

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('presentation'));
 
 const port = process.env.PORT || 3000;
 
