db5668e70322242b738df8e8433e1ac93a5e3f18
borm <efim4eg@gmail.com>
2016-07-26 20:49:39 +0300

    styl -> less

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -17,18 +17,34 @@ module.exports = (APP) => ({
     new webpack.NoErrorsPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      include: path.join(__dirname, 'apps'),
-      loaders: ['babel']
-    }, {
-      test: /\.styl$/,
-      include: path.join(__dirname, 'styles'),
-      loader: 'style-loader!css-loader!stylus-loader'
-    }, {
-      test: /\.json$/,
-      include: path.join(__dirname, 'data'),
-      loader: 'json'
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, 'apps'),
+        loaders: ['babel']
+      }, {
+        test: /\.styl$/,
+        include: path.join(__dirname, 'styles'),
+        loaders: [
+          'style','css',
+          'stylus?paths=' + path.resolve(__dirname, 'node_modules/bootstrap-styl')
+        ]
+      }, {
+        test: /\.less$/,
+        include: path.join(__dirname, 'styles'),
+        loaders: [
+          'style','css',
+          'less?paths=' + path.resolve(__dirname, 'node_modules/bootstrap/less')
+        ]
+      }, {
+        test: /\.json$/,
+        include: path.join(__dirname, 'data'),
+        loader: 'json'
+      },
+      {
+        test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+        loader: 'file'
+      }
+    ]
   }
 });
